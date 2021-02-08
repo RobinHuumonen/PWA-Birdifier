5fa9ed205fe2a13892a1095140e1e7d2c8cce094
RobinHuumonen <robin.huumonen@hotmail.com>
2021-02-08 16:15:58 +0000

    Cleaned up prefilled files, added tittle and favicon.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    document.title = "PWA-Birdifier"
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="site-wrapper">
+      <h1>Heello</h1>
     </div>
   );
 }
