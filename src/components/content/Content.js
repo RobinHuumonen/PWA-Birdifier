29a95cf55c75fce4e8de2cd7fb82bb8373043fce
RobinHuumonen <robin.huumonen@hotmail.com>
2021-02-15 18:43:22 +0000

    Onclick for classification

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -8,32 +8,50 @@ function Content() {
     { name:  'Bald Eagle', value: 90 },
     { name:  'Golden Eagle', value: 7 },
     { name:  'Turkey Vulture', value: 3 },
-  ]); 
+  ]);
+  const [renderResults, setRenderResults] = useState(false);
+
+  const classify = () => {
+    setTimeout(() => {
+      setRenderResults(true);
+    }, 4000);
+  };
+
+  const resultStyle = {
+    backgroundColor: '#273469',   
+    textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  };
+
   return (
     <ContentWrap>
+      {renderResults === true ? 
+        <div style={resultStyle}>
+        <PieChart width={400} height={400}>
+        <Pie
+        dataKey="value"
+        isAnimationActive={true}
+        data={classifications}
+        cx={200}
+        cy={200}
+        outerRadius={80}
+        fill="#8884d8"
+        label
+        />
+        <Tooltip />
+        </PieChart>
+        </div>
+      : null}
       <div className="selectImage">
        <img src={preloadedImg}></img>
       </div>
       <div className="btn-group">
         <button>Select file</button>
         <button>Take photo</button>
-        <button>Classify</button>
+        <button onClick={() => classify()}>Classify</button>
       </div>
-{/*       <div className="quideResults">
-        <PieChart width={400} height={400}>
-          <Pie
-            dataKey="value"
-            isAnimationActive={true}
-            data={classifications}
-            cx={200}
-            cy={200}
-            outerRadius={80}
-            fill="#8884d8"
-            label
-          />
-          <Tooltip />
-        </PieChart>
-      </div> */}
     </ContentWrap>
   );
 }
