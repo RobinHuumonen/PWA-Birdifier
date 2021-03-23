355214118f1e6a0ffefa474d8827de34ce541ee7
RobinHuumonen <robin.huumonen@hotmail.com>
2021-03-23 12:54:07 +0000

    background image location change

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
+import backgroundSrc from './components/resources/pexels-roberto-shumski-1903702.jpg';
 
 export const PageWrap = styled.div`
   min-height:100vh;
   min-width: 100vw;
 	font-size: 10px;   /*1rem = 10px*/
   /* background-color: #FAFAFF; */
-  background-image: url('pexels-roberto-shumski-1903702.jpg');
+  /* background-image: url('pexels-roberto-shumski-1903702.jpg'); */
+  background-image: url(${backgroundSrc});
   background-repeat: no-repeat;
   background-position: center;
   background-size: 100% 100%;
