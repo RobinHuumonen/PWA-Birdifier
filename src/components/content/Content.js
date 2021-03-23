178a2b4f7cd060cea5ea1dd88b4a8f5a2afc7a51
RobinHuumonen <robin.huumonen@hotmail.com>
2021-03-23 14:13:15 +0000

    Hotfix

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -53,11 +53,7 @@ function Content() {
   function rescaleImage(image) {
     return image.expandDims(0).toFloat().div(127).sub(1);
   }
-
-  function resizeImage(image) {
-    return tf.image.resizeBilinear(image, [pixelHeight, pixelWidth]);
-  }
-
+  
   function setThreeBestPredictions(probabilities) {
     const mapProbabilitiesToSpecies = [];
     for (let i = 0; i < probabilities.length; i++) {
@@ -94,10 +90,10 @@ function Content() {
     if (model) {
       setRenderSpinner(true);
       const image = new Image(pixelHeight, pixelWidth);
+      image.src = imgSrc;
       try {
         const img = tf.browser.fromPixels(image);
-        const resizedImage = resizeImage(img);
-        const batchedImage = rescaleImage(resizedImage);
+        const batchedImage = rescaleImage(img);
         const predict = model.predict(batchedImage);
         const probabilities = await predict.data();
         setThreeBestPredictions(probabilities);
